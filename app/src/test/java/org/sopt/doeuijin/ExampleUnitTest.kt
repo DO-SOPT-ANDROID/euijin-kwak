272bb762f857a68cea947d981fa43ffd09d3ea81
KwakEuiJin <yuijin0909@gmail.com>
2023-10-14 11:58:24 +0900

    feat(test): Add Test Code

--- app/src/test/java/org/sopt/doeuijin/ExampleUnitTest.kt
+++ app/src/test/java/org/sopt/doeuijin/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package org.sopt.doeuijin
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.sopt.common.extension.isValidLength
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,10 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testIsValidLength() {
+        assertTrue("123456".isValidLength(6, 10))
+        assertFalse("12345".isValidLength(6, 10))
+        assertTrue("1234567890".isValidLength(6, 10))
+        assertFalse("12345678901".isValidLength(6, 10))
     }
 }
