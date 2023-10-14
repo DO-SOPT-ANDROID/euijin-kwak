fb621b14185a37d50b38920959da04176ef93eab
KwakEuiJin <yuijin0909@gmail.com>
2023-10-14 11:58:50 +0900

    feat(test): Edit Lint

--- app/src/test/java/org/sopt/doeuijin/ExampleUnitTest.kt
+++ app/src/test/java/org/sopt/doeuijin/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package org.sopt.doeuijin
 
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 import org.sopt.common.extension.isValidLength
 
 /**
