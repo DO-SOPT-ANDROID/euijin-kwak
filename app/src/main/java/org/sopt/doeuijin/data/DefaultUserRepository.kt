6d4d6fb31eda70cdbad505d33864a3456514e041
KwakEuiJin <yuijin0909@gmail.com>
2023-10-20 16:50:09 +0900

    feat(suspend): fix suspend code

--- app/src/main/java/org/sopt/doeuijin/data/DefaultUserRepository.kt
+++ app/src/main/java/org/sopt/doeuijin/data/DefaultUserRepository.kt
@@ -3,50 +3,37 @@ package org.sopt.doeuijin.data
 import androidx.core.content.edit
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.suspendCancellableCoroutine
 import org.sopt.common.key.SharedPreferenceKey
 import org.sopt.doeuijin.container.sharedPreferences
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class DefaultUserRepository {
 
-    suspend fun setAutoLogin(isAutoLogin: Boolean): Boolean {
+    suspend fun setAutoLogin(isAutoLogin: Boolean) {
         delay(500)
-        return suspendCancellableCoroutine { continuation ->
-            commitAutoLogin(isAutoLogin)
-            continuation.resume(true, null)
-        }
+        commitAutoLogin(isAutoLogin)
     }
 
     suspend fun checkAutoLogin(): Boolean {
         delay(500)
-        return suspendCancellableCoroutine { continuation ->
-            val isAutoLogin = sharedPreferences.getBoolean(SharedPreferenceKey.AUTO_LOGIN, false)
-            continuation.resume(isAutoLogin, null)
-        }
+        return sharedPreferences.getBoolean(SharedPreferenceKey.AUTO_LOGIN, false)
     }
 
     suspend fun setUserIdentifier(id: String, pw: String, name: String) {
-        delay(500)
-        suspendCancellableCoroutine { continuation ->
-            sharedPreferences.edit(commit = true) {
-                putString(SharedPreferenceKey.USER_ID, id)
-                putString(SharedPreferenceKey.USER_PW, pw)
-                putString(SharedPreferenceKey.USER_NAME, name)
-            }
-            continuation.resume(Unit, null)
+        sharedPreferences.edit(commit = true) {
+            putString(SharedPreferenceKey.USER_ID, id)
+            putString(SharedPreferenceKey.USER_PW, pw)
+            putString(SharedPreferenceKey.USER_NAME, name)
         }
+        delay(500)
     }
 
     suspend fun getUserIdentifier(): Triple<String, String, String> {
+        val userId = sharedPreferences.getString(SharedPreferenceKey.USER_ID, null).orEmpty()
+        val userPw = sharedPreferences.getString(SharedPreferenceKey.USER_PW, null).orEmpty()
+        val userName = sharedPreferences.getString(SharedPreferenceKey.USER_NAME, null).orEmpty()
         delay(500)
-        return suspendCancellableCoroutine { continuation ->
-            val userId = sharedPreferences.getString(SharedPreferenceKey.USER_ID, null).orEmpty()
-            val userPw = sharedPreferences.getString(SharedPreferenceKey.USER_PW, null).orEmpty()
-            val userName =
-                sharedPreferences.getString(SharedPreferenceKey.USER_NAME, null).orEmpty()
-            continuation.resume(Triple(userId, userPw, userName), null)
-        }
+        return Triple(userId, userPw, userName)
     }
 
     private fun commitAutoLogin(isAutoLogin: Boolean) {
